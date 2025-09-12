9fa4ea302b9b1e9c299a1d3c5c7e67f962d37fd2
Octanoz <130052848+Octanoz@users.noreply.github.com>
2025-09-12 13:04:24 +0200

    Optimized Day 11

--- Day11/HexTraveler.cs
+++ Day11/HexTraveler.cs
@@ -8,52 +8,52 @@ public static class HexTraveler
     {
         Coord current = Coord.Zero;
         string[] directions = input.Split(',');
+
         foreach (var direction in directions)
         {
-            current = direction switch
-            {
-                "n" => current.Up,
-                "ne" => current.UpRight,
-                "e" => current.Right,
-                "se" => current.DownRight,
-                "s" => current.Down,
-                "sw" => current.DownLeft,
-                "w" => current.Left,
-                "nw" => current.UpLeft,
-                _ => throw new ArgumentException($"Unknown direction {direction}")
-            };
+            current = Move(current, direction);
         }
 
-        int row = Math.Abs(current.Row);
-        int col = Math.Abs(current.Col);
-
-        return row > col ? row : col;
+        return CalculateResult(current);
     }
 
     public static int Part2(string input)
     {
         Coord current = Coord.Zero;
-        int maxRow = 0, maxCol = 0;
         string[] directions = input.Split(',');
+
+        int maxRow = 0, maxCol = 0;
         foreach (var direction in directions)
         {
-            current = direction switch
-            {
-                "n" => current.Up,
-                "ne" => current.UpRight,
-                "e" => current.Right,
-                "se" => current.DownRight,
-                "s" => current.Down,
-                "sw" => current.DownLeft,
-                "w" => current.Left,
-                "nw" => current.UpLeft,
-                _ => throw new ArgumentException($"Unknown direction {direction}")
-            };
-
+            current = Move(current, direction);
             maxRow = Math.Max(maxRow, Math.Abs(current.Row));
             maxCol = Math.Max(maxCol, Math.Abs(current.Col));
         }
 
-        return maxRow > maxCol ? maxRow : maxCol;
+        return CalculateResult(new(maxRow, maxCol));
+    }
+
+    private static Coord Move(Coord current, string direction)
+    {
+        return direction switch
+        {
+            "n" => current.Up,
+            "ne" => current.UpRight,
+            "e" => current.Right,
+            "se" => current.DownRight,
+            "s" => current.Down,
+            "sw" => current.DownLeft,
+            "w" => current.Left,
+            "nw" => current.UpLeft,
+            _ => throw new ArgumentException($"Unknown direction {direction}")
+        };
+    }
+
+    private static int CalculateResult(Coord current)
+    {
+        int row = Math.Abs(current.Row);
+        int col = Math.Abs(current.Col);
+
+        return row > col ? row : col;
     }
 }
